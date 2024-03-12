5393954ed8c2b8402d817bbc48dd67fc7d0cf0d7
HP <HP@DESKTOP-U9QQCPP>
2024-03-12 20:00:20 +0530

    forth commit

---
+++ servlets/src/main/java/com/jspiders/servlets/servlet/MyServlet1.java
@@ -0,0 +1,23 @@
+package com.jspiders.servlets.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class MyServlet1 extends GenericServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+//		System.out.println("Hello World!");
+		arg1.setContentType("text/html");
+		PrintWriter writer=arg1.getWriter();
+		writer.println("<h1>Hello World!!</h1>");
+	}
+
+}
